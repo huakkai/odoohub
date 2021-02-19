780a69ed2a1a3c682e26b27486ec81d5ca9d8177
huakkai <huaqiangyan@163.com>
2021-02-19 22:26:01 +0800

    [IMP]创建模块基础结构

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'odoohub',
+    'version': '1.0',
+    'category': 'forum',
+    'sequence': 1,
+    'summary': '',
+    'description': "",
+    'website': 'https://www.odoohub.com/',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'css': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
